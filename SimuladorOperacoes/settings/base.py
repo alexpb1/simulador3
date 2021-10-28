ba5181acc41a89690703c0a2a48e424b46e06120
alexpb1 <alexsandropb3@gmail.com>
2021-10-28 18:37:59 -0300

    quarta tentativa de deploy na quinta a noite

--- SimuladorOperacoes/settings/base.py
+++ SimuladorOperacoes/settings/base.py
@@ -115,7 +115,7 @@ STATIC_ROOT = BASE_DIR/ 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'SimuladorOperacoes/static')
+    os.path.join(BASE_DIR, 'PronafMaisAlimentos/static')
 ]
 	
 # Default primary key field type

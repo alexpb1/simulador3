4d52e7161edc86ff3488b6b1421e21fb6d601b4b
alexpb1 <alexsandropb3@gmail.com>
2021-10-28 18:44:48 -0300

    quinta tentativa de deply na quinta a noite

--- SimuladorOperacoes/settings/base.py
+++ SimuladorOperacoes/settings/base.py
@@ -114,9 +114,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/ 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'PronafMaisAlimentos/static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'PronafMaisAlimentos/static')
+#]
+
 	
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

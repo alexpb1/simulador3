145b4a7d721ecb41fda5fcc347619882820a03e8
alexpb1 <alexsandropb3@gmail.com>
2021-10-28 20:34:42 -0300

    desfazendo a tentiva de colocar o site no ar 10

--- SimuladorOperacoes/settings/base.py
+++ SimuladorOperacoes/settings/base.py
@@ -111,9 +111,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = BASE_DIR/ 'static'
+STATIC_ROOT = BASE_DIR/ 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

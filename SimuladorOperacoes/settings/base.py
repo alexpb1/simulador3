6cd9b56d7649092d6c3fd9526e6ab3e5b9074c51
alexpb1 <alexsandropb3@gmail.com>
2021-10-28 19:01:06 -0300

    pronto para deploy finalmente

--- SimuladorOperacoes/settings/base.py
+++ SimuladorOperacoes/settings/base.py
@@ -114,11 +114,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/ 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'PronafMaisAlimentos/static')
-#]
 
-	
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

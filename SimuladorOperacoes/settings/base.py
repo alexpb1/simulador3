5362b454fb0879336e7e6c18898ed926b1a71cfa
alexpb1 <alexsandropb3@gmail.com>
2021-10-28 19:37:36 -0300

    testando fazer o site funcionar 1

--- SimuladorOperacoes/settings/base.py
+++ SimuladorOperacoes/settings/base.py
@@ -107,12 +107,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = BASE_DIR/ 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
